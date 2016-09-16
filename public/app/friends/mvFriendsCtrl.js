e3166f60b017631c1f80e2e84396c8eb8877c22e
cogo79 <migobigo76@gmail.com>
2016-09-16 23:39:15 +0200

    Friends list progress. It is made responsive and clickable

--- public/app/friends/mvFriendsCtrl.js
+++ public/app/friends/mvFriendsCtrl.js
@@ -1,6 +1,15 @@
 angular.module('app').controller('mvFriendsCtrl',['$scope', 'mvFriend', 'mvCachedFriends', function($scope, mvFriend, mvCachedFriends) {
-
+	$scope.friends;
 	mvCachedFriends.query().$promise.then(function(friends) {
-		console.log(friends);
+		$scope.friends = friends;
 	});
+
+	$scope.gotoProfile = function(_id) {
+		console.log("gotoProfile id:"+_id);
+	}
+
+	$scope.delete = function($event, _id) {
+		$event.stopPropagation();
+		console.log("delete id:"+_id);
+	}
 }]);
