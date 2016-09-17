4d79e4e60100c51eb1fc94dfcb3f321a13170f34
cogo79 <migobigo76@gmail.com>
2016-09-17 15:36:06 +0200

    Profile page ready and also made responsive

--- public/app/friends/mvProfileCtrl.js
+++ public/app/friends/mvProfileCtrl.js
@@ -1,9 +1,12 @@
 angular.module('app').controller('mvProfileCtrl',['$scope', 'mvFriend', '$routeParams', '$location', function($scope, mvFriend, $routeParams, $location) {
+	
 	$scope.friend;
+
 	mvFriend.get({id:$routeParams.id}).$promise.then(function(friend) {
 		console.log(friend);
 		$scope.friend = friend;
 	}, function(error) {
 		$location.path('/');
 	});
+
 }]);
